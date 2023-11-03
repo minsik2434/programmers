507fe9501325f65b3cc74d7fc826ce8297e56f40
minsik2434 <119111149+minsik2434@users.noreply.github.com>
2023-11-03 12:07:48 +0900

    [level unrated] Title: 카운트 다운, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -2,13 +2,9 @@ import java.util.ArrayList;
 class Solution {
     public int[] solution(int start, int end_num) {
         int[] answer = {};
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i=start; i>= end_num; i--){
-            list.add(i);
-        }
-        answer = new int[list.size()];
-        for(int i=0; i<list.size(); i++){
-            answer[i] = list.get(i);
+        answer = new int[start-end_num+1];
+        for(int i=0; i<=start-end_num; i++){
+            answer[i] = start-i;
         }
         return answer;
     }
