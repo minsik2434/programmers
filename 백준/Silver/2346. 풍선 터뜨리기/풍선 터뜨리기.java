34b77461ce5815f18f8ae08f3bcbce2ae7ef3dd7
minsik2434 <119111149+minsik2434@users.noreply.github.com>
2024-02-12 15:47:00 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 176 ms, Memory: 15156 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -10,32 +10,28 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         int num = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[num];
         for(int i=0; i<num; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+            deque.offerLast(new int[]{i,Integer.parseInt(st.nextToken())});
         }
+        int[] value = deque.pollFirst();
         sb.append("1 ");
-        int start = arr[0];
-        for(int i=1; i<num; i++){
-            deque.offerLast(new int[] {(i+1), arr[i]});
-        }
-
+        int start = value[1];
         while(!deque.isEmpty()){
             if(start > 0){
                 for(int i=1; i<start; i++){
                     deque.offerLast(deque.pollFirst());
                 }
-                int[] value = deque.pollFirst();
+                value = deque.pollFirst();
                 start = value[1];
-                sb.append(value[0]).append(" ");
+                sb.append(value[0]+1).append(" ");
             }
             else{
                 for(int i=1; i<-start; i++){
                     deque.offerFirst(deque.pollLast());
                 }
-                int[] value = deque.pollLast();
+                value = deque.pollLast();
                 start = value[1];
-                sb.append(value[0]).append(" ");
+                sb.append(value[0]+1).append(" ");
             }
         }
         System.out.println(sb);
