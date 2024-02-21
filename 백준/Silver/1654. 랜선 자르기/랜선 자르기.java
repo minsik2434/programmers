98fa66d66c95cfd339b3599e267af18754ee96fa
최민식 <119111149+minsik2434@users.noreply.github.com>
2024-02-21 15:03:19 +0900

    [Silver II] Title: 랜선 자르기, Time: 184 ms, Memory: 17412 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -6,33 +6,31 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[K];
         long max = 0;
-        int[] arr = new int[N];
-        for(int i=0; i<N; i++){
+        for(int i=0; i<K; i++){
             arr[i] = Integer.parseInt(br.readLine());
             if(max < arr[i]){
                 max = arr[i];
             }
         }
-        max++;
+        max = max + 1;
         long min = 0;
-        long mid = 0;
         while(min<max){
+            long mid = (max+min)/2;
             long count = 0;
-            mid = (min+max)/2;
             for(int i=0; i<arr.length; i++){
                 count += arr[i]/mid;
             }
 
-            if(count < M){
+            if(count < N){
                 max = mid;
             }
             else{
-                min = mid+1;
+                min = mid + 1;
             }
         }
         System.out.println(min-1);
