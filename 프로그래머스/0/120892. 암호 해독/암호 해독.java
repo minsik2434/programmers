5c0e3b58e48e66122b5b836b4b2c0378c5767b47
최민식 <119111149+minsik2434@users.noreply.github.com>
2024-08-05 19:44:47 +0900

    [level 0] Title: 암호 해독, Time: 21.87 ms, Memory: 91.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,12 @@
+class Solution {
+public String solution(String cipher, int code) {
+            String answer = "";
+            char[] arr = cipher.toCharArray();
+            for(int i=0; i<arr.length; i++){
+                if((i+1)%code == 0){
+                    answer += arr[i];
+                }
+            }
+            return answer;
+        }
+}
