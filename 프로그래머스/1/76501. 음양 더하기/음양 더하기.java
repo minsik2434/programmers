3388b7d0439a6b66a3cf131c8d37d517e90be320
최민식 <119111149+minsik2434@users.noreply.github.com>
2024-08-08 16:29:35 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+            int answer = 123456789;
+            int sum = 0;
+            for(int i=0; i<absolutes.length; i++){
+                if(signs[i]){
+                    sum = sum + absolutes[i];
+                }
+                else {
+                    sum = sum - absolutes[i];
+                }
+            }
+            answer = sum;
+            return answer;
+        }
+}
