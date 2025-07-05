e4a386336e0fb4474c941f76e8d5f51fc3fa6492
최민식 <119111149+minsik2434@users.noreply.github.com>
2025-07-05 21:11:28 +0900

    [Silver III] Title: 소수 구하기, Time: 592 ms, Memory: 30852 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -4,37 +4,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void eratosthenesSieve(int[] arr, int m, int n){
-        for(int i = 2; i <= n; i++){
-            arr[i] = i;
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
 
-        for(int i = 2; i <= n; i++){
-            // i = 4 일때는 i=2에서 0으로 초기화 됬으니까 continue..
-            if(arr[i] == 0) {
-                continue;
-            }
-            // i = 2 이면 2 부터 n까지 2, 2+2, 4+2, 8+2 ...
-            for(int j = i + i; j <= n; j += i){
-                arr[j] = 0;
+        boolean[] flag = new boolean[n+1];
+        prime(flag, n);
+        for(int i=m; i<=n; i++){
+            if(flag[i]){
+                System.out.println(i);
             }
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    static void prime(boolean[] flag, int n){
 
-        int m = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
+        for (int i=2; i<=n; i++){
+            flag[i] = true;
+        }
 
-        int[] arr = new int[n+1];
-        eratosthenesSieve(arr, m, n);
-
-        for(int i=m; i<=n; i++){
-            if(arr[i] != 0){
-                System.out.println(arr[i]);
+        for(int i=2; i*i <= n; i++){
+            if(flag[i]){
+                for(int j=i+i; j<=n; j+=i){
+                    flag[j] = false;
+                }
             }
         }
+
     }
 }
