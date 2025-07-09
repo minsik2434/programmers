392b5c207f6acab354187c42c2f8e9d6737b6d18
최민식 <119111149+minsik2434@users.noreply.github.com>
2025-07-09 20:21:50 +0900

    [Bronze V] Title: 사칙연산, Time: 180 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        int a = stdin.nextInt();
+        int b = stdin.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
