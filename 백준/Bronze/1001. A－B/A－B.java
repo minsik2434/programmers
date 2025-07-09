8758ae6a454bd906b6c1e17811866a9b3a780f9e
최민식 <119111149+minsik2434@users.noreply.github.com>
2025-07-09 20:10:35 +0900

    [Bronze V] Title: A-B, Time: 180 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        int a = stdin.nextInt();
+        int b = stdin.nextInt();
+        System.out.println(a-b);
+    }
+}
