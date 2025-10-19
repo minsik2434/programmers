aaac26bb696467fd5432b9ad1652080d045a1aa5
최민식 <119111149+minsik2434@users.noreply.github.com>
2025-10-19 21:16:02 +0900

    [Silver V] Title: 색종이, Time: 180 ms, Memory: 17864 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -3,20 +3,27 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int count = 0;
         int n = sc.nextInt();
         boolean[][] paper = new boolean[101][101];
         for(int i=0; i<n; i++){
             int x = sc.nextInt();
             int y = sc.nextInt();
-            for(int j=x; j<x+10; j++){
-                for(int k=y; k<y+10; k++){
-                    paper[j][k] = true;
-                }
+            paint(x, y, paper);
+        }
+        int result = totalExtend(paper);
+        System.out.println(result);
+    }
+
+    static void paint(int x, int y, boolean[][] paper){
+        for(int i=x; i<x+10; i++){
+            for(int j=y; j<y+10; j++){
+                paper[i][j] = true;
             }
         }
+    }
 
+    static int totalExtend(boolean[][] paper){
+        int count = 0;
         for(int i=0; i<101; i++){
             for(int j=0; j<101; j++){
                 if(paper[i][j]){
@@ -25,8 +32,6 @@ public class Main {
             }
         }
 
-
-        System.out.println(count);
-
+        return count;
     }
 }
