8bd588863a5f433f9fd2ebca9ae747b2400df711
최민식 <119111149+minsik2434@users.noreply.github.com>
2025-10-20 23:53:45 +0900

    [Silver V] Title: 방 번호, Time: 168 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,33 +1,30 @@
 import java.util.Scanner;
-
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int solve = solve(n);
-        System.out.println(solve);
-    }
-
-    static int solve(int n){
         int[] arr = new int[10];
-        while(n > 0){
-            int i = n % 10;
-            arr[i] ++;
-            n /= 10;
+        String s = sc.nextLine();
+
+        for(int i=0; i<s.length(); i++){
+            int num = s.charAt(i) - '0';
+            arr[num]++;
         }
 
         int sixAndNine = arr[6] + arr[9];
         int max = (sixAndNine + 1) / 2;
-
         for(int i=0; i<arr.length; i++){
-            if(i == 6 || i == 9){
+            if(i == 6){
                 continue;
             }
-            if(arr[i] > max){
+            if (i == 9){
+                continue;
+            }
+
+            if (max < arr[i]){
                 max = arr[i];
             }
         }
 
-        return max;
+        System.out.println(max);
     }
 }
