72701a7ec5aeabb87f3e90dc5cee4ff88663ebff
최민식 <119111149+minsik2434@users.noreply.github.com>
2025-10-27 21:10:21 +0900

    [Bronze I] Title: 평균, Time: 220 ms, Memory: 18832 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double[] arr = new double[n];
+        double max = 0;
+        double sum = 0;
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        for(int i=0; i<arr.length; i++){
+            arr[i] = arr[i]/max * 100;
+            sum += arr[i];
+        }
+
+        System.out.println(sum/arr.length);
+    }
+}
