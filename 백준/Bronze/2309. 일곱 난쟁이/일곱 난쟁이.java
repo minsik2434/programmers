44722849b531f6e7eb117f1d8981d2db56fde53a
최민식 <119111149+minsik2434@users.noreply.github.com>
2025-11-03 23:47:24 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 176 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[9];
+        int sum = 0;
+
+        int fake1 = 0;
+        int fake2 = 0;
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(sum - arr[i] - arr[j] == 100){
+                    fake1 = i;
+                    fake2 = j;
+                }
+            }
+        }
+
+        for(int i=0; i<arr.length; i++){
+            if(i != fake1 && i != fake2){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
