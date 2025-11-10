dc8062b2e953c6a0cfba781b11c2f8df9a84cae1
최민식 <119111149+minsik2434@users.noreply.github.com>
2025-11-10 16:12:58 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,22 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] arr = new int[9];
         int sum = 0;
-
-        int fake1 = 0;
-        int fake2 = 0;
-        for(int i=0; i<arr.length; i++){
-            arr[i] = sc.nextInt();
+        for(int i=0; i<9; i++){
+            arr[i] = Integer.parseInt(br.readLine());
             sum += arr[i];
         }
 
+        solve(arr, sum);
+
+    }
+
+    static void solve(int[] arr, int sum){
+        int fake1 = 0;
+        int fake2 = 0;
+
         Arrays.sort(arr);
-        for(int i=0; i<arr.length; i++){
-            for(int j=i+1; j<arr.length; j++){
+        for(int i=0; i<8; i++){
+            for(int j=i+1; j<9; j++){
                 if(sum - arr[i] - arr[j] == 100){
                     fake1 = i;
                     fake2 = j;
@@ -25,11 +32,12 @@ public class Main {
             }
         }
 
-        for(int i=0; i<arr.length; i++){
-            if(i != fake1 && i != fake2){
-                System.out.println(arr[i]);
+        for(int i=0; i<9; i++){
+            if (i == fake1 || i == fake2){
+                continue;
             }
-        }
 
+            System.out.println(arr[i]);
+        }
     }
 }
