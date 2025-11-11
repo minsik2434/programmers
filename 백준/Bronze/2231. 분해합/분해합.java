c0df20bc428f9044fbc4db7ad73751b1efb48268
최민식 <119111149+minsik2434@users.noreply.github.com>
2025-11-11 14:11:06 +0900

    [Bronze II] Title: 분해합, Time: 128 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,31 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int result = constructor(n);
-        System.out.println(result);
-    }
 
-    static int constructor(int n){
         int result = 0;
         for(int i=1; i<n; i++){
-            int sum = i;
             int value = i;
-            while(value > 0) {
+            int sum = i;
+            while(value > 0){
                 sum += value%10;
                 value /= 10;
             }
-            
-            if (sum == n){
+
+            if(sum == n){
                 result = i;
                 break;
             }
         }
-        
-        return result;
+
+        System.out.println(result);
+
     }
 }
