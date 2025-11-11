1febbbf6c7d070711970afb052176cb6ba01c351
최민식 <119111149+minsik2434@users.noreply.github.com>
2025-11-11 17:50:10 +0900

    [Silver IV] Title: 설탕 배달, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -4,25 +4,52 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int solve = solve(n);
-        System.out.println(solve);
+//        int result = bruteForce(n);
+        int result = dp(n);
+
+        System.out.println(result);
+
     }
 
-    static int solve(int n){
+    static int bruteForce(int n){
+        int count = 0;
+        while(n>0){
+            if(n % 5 == 0){
+                count += n/5;
+                break;
+            }
+            else {
+                n -= 3;
+                count++;
+            }
+
+            if(n < 0){
+                count = -1;
+            }
+        }
+
+        return count;
+    }
+
+    static int dp(int n){
         int[] dp = new int[n+1];
+
+        dp[0] = -1;
         dp[1] = -1;
         dp[2] = -1;
-        for(int i=3; i<=n; i++){
-            if(i%5 == 0){
-                dp[i] = i/5;
-                continue;
-            }
-            if(dp[i-3] == -1){
+        dp[3] = 1;
+
+        for(int i=4; i<=n; i++) {
+            if (i % 5 == 0) {
+                dp[i] = i / 5;
+            } else if (dp[i - 3] == -1) {
                 dp[i] = -1;
-                continue;
+            } else {
+                dp[i] = dp[i - 3] + 1;
             }
-            dp[i] = dp[i-3] + 1;
         }
+
         return dp[n];
     }
+
 }
