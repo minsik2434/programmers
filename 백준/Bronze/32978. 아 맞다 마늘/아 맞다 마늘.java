ddbb8fc52be95564969efe1550ab23119a4c8106
최민식 <119111149+minsik2434@users.noreply.github.com>
2025-11-17 23:29:25 +0900

    [Bronze III] Title: 아 맞다 마늘, Time: 268 ms, Memory: 19488 KB -BaekjoonHub

--- 백준/Bronze/32978. 아 맞다 마늘/아 맞다 마늘.java
+++ 백준/Bronze/32978. 아 맞다 마늘/아 맞다 마늘.java
@@ -4,25 +4,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String[] arr = new String[n];
-        String[] arr2 = new String[n-1];
+        String[] target = new String[n];
+        String[] lookup = new String[n-1];
         for(int i=0; i<n; i++){
-            arr[i] = sc.next();
+            target[i] = sc.next();
         }
         for(int i=0; i<n-1; i++){
-            arr2[i] = sc.next();
+            lookup[i] = sc.next();
         }
 
-        for(int i=0; i<n; i++){
+        int indexNotOnArray = findIndexNotOnArray(target, lookup);
+
+        System.out.println(target[indexNotOnArray]);
+    }
+
+    static int findIndexNotOnArray(String[] target, String[] lookup){
+        int result = -1;
+        for(int i=0; i<target.length; i++){
             int count = 0;
-            for(int j=0; j<n-1; j++){
-                if(arr[i].equals(arr2[j])){
+            for(int j=0; j<lookup.length; j++){
+                if (target[i].equals(lookup[j])){
                     count++;
+                    break;
                 }
             }
-            if(count == 0){
-                System.out.println(arr[i]);
+            if (count == 0){
+                result = i;
+                break;
             }
         }
+        return result;
     }
 }
+
