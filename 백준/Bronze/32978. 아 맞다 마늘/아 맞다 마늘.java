f546f6b256a325b848e98c54aa9d2f880a495115
최민식 <119111149+minsik2434@users.noreply.github.com>
2025-11-17 23:20:21 +0900

    [Bronze III] Title: 아 맞다 마늘, Time: 292 ms, Memory: 19444 KB -BaekjoonHub

---
+++ 백준/Bronze/32978. 아 맞다 마늘/아 맞다 마늘.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+        String[] arr2 = new String[n-1];
+        sc.nextLine();
+        for(int i=0; i<n; i++){
+            arr[i] = sc.next();
+        }
+        sc.nextLine();
+        for(int i=0; i<n-1; i++){
+            arr2[i] = sc.next();
+        }
+
+        for(int i=0; i<n; i++){
+            int count = 0;
+            for(int j=0; j<n-1; j++){
+                if(arr[i].equals(arr2[j])){
+                    count++;
+                }
+            }
+            if(count == 0){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
+
