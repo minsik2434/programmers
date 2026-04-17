37773ecd0e38c7391b392aacb3cba1372a45d07e
최민식 <119111149+minsik2434@users.noreply.github.com>
2026-04-17 18:32:29 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,17 +1,19 @@
 import java.util.HashMap;
 import java.util.Map;
+
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 0;
         Map<String, Integer> map = new HashMap<>();
         for(int i=0; i<clothes.length; i++) {
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 1) + 1);
         }
-        int tmp = 1;
-        for (Integer value : map.values()) {
-            tmp *= (value+1);
+
+        int sum = 1;
+        for(String type : map.keySet()) {
+            sum *= map.get(type);
         }
-        answer = tmp - 1;
+        answer = sum - 1;
         return answer;
     }
 }
