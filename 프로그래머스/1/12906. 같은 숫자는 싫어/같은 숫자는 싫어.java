5502289f9f4943200b18e763e283ef7d647a6b77
최민식 <119111149+minsik2434@users.noreply.github.com>
2026-04-18 23:36:11 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 46.41 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,19 +3,15 @@ import java.util.*;
 public class Solution {
     public int[] solution(int []arr) {
         Deque<Integer> queue = new ArrayDeque<>();
-        queue.offer(arr[0]);
-        for(int i=1; i<arr.length; i++) {
-            if(queue.peekLast() == arr[i]){
-                continue;
+        for(int i=0; i<arr.length; i++) {
+            if (queue.isEmpty() || queue.peekLast() != arr[i]) {
+                queue.offer(arr[i]);
             }
-            queue.offer(arr[i]);
         }
         int[] answer = new int[queue.size()];
-
         for(int i=0; i<answer.length; i++) {
             answer[i] = queue.poll();
         }
-
         return answer;
     }
 }
