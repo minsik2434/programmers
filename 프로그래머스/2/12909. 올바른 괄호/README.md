3c1650b09379dec9a641f6f8a7f01022c1a93234
최민식 <119111149+minsik2434@users.noreply.github.com>
2026-04-21 02:49:11 +0900

    [level 2] Title: 올바른 괄호, Time: 19.28 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
 
 ### 성능 요약
 
-메모리: 55.1 MB, 시간: 19.93 ms
+메모리: 54.4 MB, 시간: 19.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 20일 19:50:54
+2026년 04월 21일 02:49:05
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,21 +3,26 @@ import java.util.Deque;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
+        //스택을 이용해 닫는 괄호인 경우 스택의 최상단의 여는 괄호를 빼는 식으로 괄호 짝을 확인
         Deque<Character> stack = new ArrayDeque<>();
         char[] charArray = s.toCharArray();
 
         for(int i=0; i<charArray.length; i++) {
+            // 괄효가 여는 괄호면 스택에 추가
             if (charArray[i] == '(') {
                 stack.push(charArray[i]);
             } else {
-                if (stack.isEmpty() || stack.peekLast() == ')') {
-                    stack.push(charArray[i]);
+                //괄호가 닫는 괄호이면서 스택이 비어있다면 올바르지 않은 괄호 조합이므로 바로 false를 리턴
+                if (stack.isEmpty()) {
+                    answer = false;
+                    break;
                 } else {
+                    // 스택의 최상단이 여는괄호이면 해당 요소를 뺌
                     stack.pop();
                 }
             }
         }
-
+        //스택이 모두 비어있다면 올바르게 괄호가 짝 지어짐
         if (!stack.isEmpty()) {
             answer = false;
         }
